14a6cccdb0437d3d6012da692fcfab286002404e
aravetmtg <aravet@makethegrade.fr>
2023-01-23 09:05:46 +0100

    fix apia + corrections bugs

--- src/js/modules/two-cols-caroussel.js
+++ src/js/modules/two-cols-caroussel.js
@@ -8,11 +8,23 @@ require('flickity-fullscreen');
 Flickity.setJQuery($);
 jQueryBridget('flickity', Flickity, $);
 
-$('.carousel-main').flickity({ 
+
+
+
+
+$('.carousel-main').flickity({
   pageDots: false,
   prevNextButtons: true,
   fullscreen: true,
   wrapAround: true,
+  on: {
+    fullscreenChange: function () {
+      console.log('Flickity changed');
+      $('.carousel-main').flickity('resize');
+      $('.carousel-nav').flickity('resize');
+
+    }
+  }
 });
 
 $('.carousel-nav').flickity({
@@ -20,4 +32,10 @@ $('.carousel-nav').flickity({
   contain: true,
   pageDots: false,
   wrapAround: true,
+  on: {
+    fullscreenChange: function () {
+      $('.carousel-main').flickity('resize');
+      $('.carousel-nav').flickity('resize');
+    }
+  }
 });
